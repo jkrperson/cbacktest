fd7085341b5ed31de986e65f34a1079ad12e8bca
Jose Kristian Resabal <jkresabal@up.edu.ph>
2022-11-10 20:11:06 +0800

    Initialized strategy abstract class

--- strategy.h
+++ strategy.h
@@ -1,5 +1,13 @@
 #ifndef _STRATEGY_H
 #define _STRATEGY_H
 
+class Strategy{
+
+    public:
+        Strategy();
+        virtual ~Strategy();
+
+        virtual calculate_signals(){} = 0;
+};
 
 #endif
